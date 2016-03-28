af066857141098622592a09714f5fbb9133f9a8a
rima1712 <rim.aifa@esprit.tn>
2016-03-29 01:09:51 +0200

    passport  commit

--- app/controllers/passport-controller.js
+++ app/controllers/passport-controller.js
@@ -4,8 +4,16 @@ var Account = require('../models/Account');
 var router = express.Router();
 
 function createUser(req, res){
+    
+    var NewAccount = {
+        
+        username : req.body.username,
+        password : req.body.password,
+        email : req.body.email,
+        codePostale : req.body.codePostale
+    }
 	
-	 Account.register(new Account({ username : req.body.username}), req.body.password,
+	 Account.register(new Account(NewAccount),
 		function(err, account) {
 			if (err) {
 				return res.status(404).send("test!");
