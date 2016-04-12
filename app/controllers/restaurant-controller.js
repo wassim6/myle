20cccd063248b5199b0a6a5a4ab7c2c85b98aa12
samarabdelkhalek <samar.abdelkhalek@esprit.tn>
2016-04-12 23:39:46 +0100

    update

--- app/controllers/restaurant-controller.js
+++ app/controllers/restaurant-controller.js
@@ -4,7 +4,7 @@ var cheerio = require('cheerio');
 var Restaurant = require('../models/Restaurant');
 var Schema = mongoose.Schema; // allows us to create a constructor for our model
 
-
+//retourner tout les restaurant
 function getAll(request, response){
     Restaurant.find({},function(error, restaurants) {
             if (error) console.error('Could not retrieve Restaurants b/c:', error);
